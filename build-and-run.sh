1e312e0cf0482b6caab899736209d6ad99a7e3bc
d.sharhorodskyi <dmytro.sharhorodskyi@90poe.io>
2019-07-31 13:52:40 +0300

    added some useful commented docker commands

--- build-and-run.sh
+++ build-and-run.sh
@@ -2,3 +2,9 @@
 
 # build image and run it with `go build` command
 docker run --rm -it $(docker build -q .) go build
+
+# bash into container
+#docker run --rm -it $(docker build -q .) bash
+
+# cat of librdkafka header files
+#docker run --rm -it $(docker build -q .) cat /usr/include/librdkafka/rdkafka.h
